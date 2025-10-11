fcc2d5581d19cc0f709c36f7aaed0a03946c45c3
Urich-loic <655808580ab@gmail.com>
2025-10-11 20:14:34 +0100

    Adding user login and logout system

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -41,5 +41,9 @@ class AuthController extends Controller
     }
 
 
-    public function logout(Request $request) {}
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json(['message' => 'Logged out successfully']);
+    }
 }
